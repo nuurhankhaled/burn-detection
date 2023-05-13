a7cedf50ac8b262643df3589668b911aba86ee16
Nuurhan Khaled <nuurhankhaled@gmail.com>
2023-05-13 19:11:24 +0300

    commit

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify, request, session
 from roboflow import Roboflow
 import os
+
 app = Flask(__name__)
 # Initialize the Roboflow instance and set the API key
 rf = Roboflow(api_key="T3mYNN9ubGhQnuAOzDfR")
 project = rf.workspace().project("skin-burn-detiction")
 model_version = 3
 model = project.version(model_version).model
-@app.route("/predict",methods=['POST'])
+
+
+@app.route("/predict", methods=['POST'])
 def predict():
     # Get the image_path parameter from the request header
     image_path = request.args.get('image_path')
@@ -16,4 +19,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
